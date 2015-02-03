b60e4ec7c743ea60298ccea469f1e14f5b238d57
Ryan Laughlin <rofreg@gmail.com>
2015-02-02 20:58:48 -0500

    Initial commit: log in with Twitter button

---
+++ PinchPad/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PinchPad
+//
+//  Created by Ryan Laughlin on 2/2/15.
+//
+//
+
+import UIKit
+import TwitterKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let logInButton = TWTRLogInButton(logInCompletion: {
+            (session: TWTRSession!, error: NSError!) in
+            // play with Twitter session
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
