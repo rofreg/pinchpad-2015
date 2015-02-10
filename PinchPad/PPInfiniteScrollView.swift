9bcd0535e44c60ef8a451ce2bbed46b108f16e23
Ryan Laughlin <rofreg@gmail.com>
2015-02-10 10:37:50 -0500

    Start work on tool menu

--- PinchPad/PPInfiniteScrollView.swift
+++ PinchPad/PPInfiniteScrollView.swift
@@ -49,32 +49,29 @@ class PPInfiniteScrollView: UIScrollView, UIScrollViewDelegate{
         // Set content size to 2x screen width, 1x screen height
         self.contentSize = CGSize(width:CGRectGetWidth(frame) * 2, height: CGRectGetHeight(frame))
         self.contentView.frame.size = self.contentSize;
-//        self.contentView.frame.size.width -= 50
-//        self.contentView.frame.size.height -= 50
     }
     
-    func scrollViewDidZoom(scrollView: UIScrollView) {
-        //zoomtorect
-        println(self.zoomScale)
-        println(self.contentView.frame.size)
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        // Scale our drawing view up and down
-        if scrollView.contentOffset.x > 999999{
-            println("TOO BIG")
-        }
-        
-        
-        var leftMargin = (self.frame.size.width - self.contentView.frame.size.width)*0.5;
-        var topMargin = (self.frame.size.height - self.contentView.frame.size.height)*0.5;
-        self.contentInset = UIEdgeInsetsMake(max(0, topMargin), max(0, leftMargin), 0, 0);
-    }
-    
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return nil
-//        return self.contentView
-    }
+//    func scrollViewDidZoom(scrollView: UIScrollView) {
+//        //zoomtorect
+//        println(self.zoomScale)
+//        println(self.contentView.frame.size)
+//    }
+//    
+//    func scrollViewDidScroll(scrollView: UIScrollView) {
+//        // Scale our drawing view up and down
+//        if scrollView.contentOffset.x > 999999{
+//            println("TOO BIG")
+//        }
+//        
+//        
+//        var leftMargin = (self.frame.size.width - self.contentView.frame.size.width)*0.5;
+//        var topMargin = (self.frame.size.height - self.contentView.frame.size.height)*0.5;
+//        self.contentInset = UIEdgeInsetsMake(max(0, topMargin), max(0, leftMargin), 0, 0);
+//    }
+//    
+//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+//        return nil   //return self.contentView
+//    }
     
     func clear(){
         self.contentView.clear()
---
+++ PinchPad/PPToolConfigurationView.swift
@@ -0,0 +1,15 @@
+//
+//  PPToolConfigurationView.swift
+//  PinchPad
+//
+//  Created by Ryan Laughlin on 2/8/15.
+//
+//
+
+import UIKit
+
+class PPToolConfigurationView: UIView{
+    @IBAction func changeWidth(){
+        
+    }
+}
