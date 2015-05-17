52a40af1cbcf9c1bdc9c83acd455c0e6db25021b
Ryan Laughlin <rofreg@gmail.com>
2015-05-17 12:35:02 -0400

    Add color picker to tool configuration

--- PinchPad/PPToolConfigurationView.swift
+++ PinchPad/PPToolConfigurationView.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class PPToolConfigurationViewController: UIViewController{
     @IBOutlet var toolPicker: UISegmentedControl!
-    @IBOutlet var slider: UISlider!     // TODO: segmented steps on the slider
+    @IBOutlet var slider: UISlider!     // TODO: show segmented steps on the slider
     @IBOutlet var previewWindow: PPCanvas!
+    @IBOutlet var colorCollectionView: UICollectionView!
+    let colors = [UIColor.blackColor(), UIColor(hex:"999999"), UIColor(hex:"dddddd"), UIColor(hex:"F2CA42"), UIColor(hex:"00C3A9"), UIColor(hex:"D45354"), UIColor(hex:"2FCAD8"), UIColor(hex:"663300"), UIColor(hex:"af7a56"), UIColor(hex:"ab7dbe"), UIColor(hex:"ff8960"), UIColor(hex:"6e99d4"), UIColor(hex:"4c996e"), UIColor(hex:"dc9bb1")]
     
     override func viewDidLoad() {
         updatePreview()
@@ -25,15 +27,12 @@ class PPToolConfigurationViewController: UIViewController{
         switch (sender.selectedSegmentIndex){
             case 0:
                 PPToolConfiguration.sharedInstance.tool = PPToolType.Brush
-                PPToolConfiguration.sharedInstance.color = UIColor.blackColor()
                 break;
             case 1:
                 PPToolConfiguration.sharedInstance.tool = PPToolType.Marker
-                PPToolConfiguration.sharedInstance.color = UIColor.redColor()
                 break;
             case 2:
                 PPToolConfiguration.sharedInstance.tool = PPToolType.Eraser
-                PPToolConfiguration.sharedInstance.color = UIColor.blueColor()
                 break;
             default:
                 PPToolConfiguration.sharedInstance.tool = PPToolType.Brush
@@ -54,6 +53,7 @@ class PPToolConfigurationViewController: UIViewController{
     // MARK: Stroke preview logic
     
     func updatePreview(){
+        previewWindow.clear()
         previewWindow.strokes = [previewStroke()]
         previewWindow.setNeedsDisplay()
         
@@ -66,6 +66,9 @@ class PPToolConfigurationViewController: UIViewController{
         } else if (tool == .Eraser && toolPicker.selectedSegmentIndex != 2){
             toolPicker.selectedSegmentIndex = 2
         }
+        
+        // Update the color picker if the color changed
+        colorCollectionView.reloadData()
     }
     
     func previewStroke() -> PPStroke{
@@ -85,5 +88,57 @@ class PPToolConfigurationViewController: UIViewController{
         stroke.addPoint(CGPointMake(windowSize * 0.9, windowSize * 0.1))
         
         return stroke
+    }
+}
+
+
+extension PPToolConfigurationViewController : UICollectionViewDataSource{
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        // Static list of colors to choose from
+        
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("colorCell", forIndexPath: indexPath) as! UICollectionViewCell
+        cell.backgroundColor = colors[indexPath.row % colors.count]
+        
+        // Highlight the currently active color
+        if (cell.backgroundColor == PPToolConfiguration.sharedInstance.color){
+            cell.layer.borderColor = UIColor.whiteColor().CGColor
+            cell.layer.borderWidth = 3
+        } else {
+            cell.layer.borderWidth = 0
+        }
+        
+        return cell
+    }
+}
+
+
+extension PPToolConfigurationViewController : UICollectionViewDelegate{
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        PPToolConfiguration.sharedInstance.color = colors[indexPath.row % colors.count]
+    }
+}
+
+
+extension UIColor{
+    // Extend UIColor to accept hex colors
+    convenience init(hex: String) {
+        let characterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet().mutableCopy() as! NSMutableCharacterSet
+        characterSet.formUnionWithCharacterSet(NSCharacterSet(charactersInString: "#"))
+        var cString = hex.stringByTrimmingCharactersInSet(characterSet).uppercaseString
+        if (count(cString) != 6) {
+            self.init(white: 1.0, alpha: 1.0)
+        } else {
+            var rgbValue: UInt32 = 0
+            NSScanner(string: cString).scanHexInt(&rgbValue)
+            
+            self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0))
+        }
     }
 }
